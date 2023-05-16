8f2df8c7fd60febf9a2f6180c5aed66a37495c6b
Morgoth228 <Dogman113636@gmail.com>
2023-05-16 16:28:40 +0300

    Добавьте файлы проекта.

---
+++ Task_6_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using MusicalInstrumentClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_6_1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            BassGuitar bassGuitar = new BassGuitar();
+            bassGuitar.Name= "Test";
+            bassGuitar.NumberOfStrings= 1;
+            Console.WriteLine(bassGuitar.Info());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
